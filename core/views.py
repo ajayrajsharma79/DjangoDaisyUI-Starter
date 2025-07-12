70ff3500bf9a7153295d5d11cca8995818ae04bc
Ajay Sharma <ajayrajsharma79@hotmail.com>
2025-07-12 10:39:07 +0530

    Initial Commit

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def landing_page(request):
+    """
+    Render the landing page.
+    """
+    return render(request, 'landing_page.html')
